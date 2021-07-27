fbcec46f6731cfb060a98988ff4761ee18d872db
K. Lange <klange@toaruos.org>
2021-07-28 08:55:33 +0900

    test

--- version.sh
+++ version.sh
@@ -2,14 +2,5 @@
 
 cat << EOF
 gwion: latest
-wren: $(wren --version)
-lua: $(lua -v | cut -d" " -f2)
-python: $(python --version [ cut -d" " -f2)
-chuck: $(chuck --version 2>&1 | head -2 | tail -1 | cut -d " " -f 3-5)
-ruby: $(ruby -v | cut -d" " -f2)
-horse64: $(horse64 --short-version)
-dictu: $(dictu -v)
-vyse: latest
-pcrap: latest
 kuroko: $(kuroko --version)
 EOF
